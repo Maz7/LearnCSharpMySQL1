90f5905d7d6e9cc43d644e8e685177a4c00ccf29
Madazer <codermaz@outlook.com>
2016-01-11 20:06:32 +0000

    Connecting to MySQL server code.
    Successful connection.

--- LearnCSharpMySQL1/Form1.cs
+++ LearnCSharpMySQL1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace LearnCSharpMySQL1
 {
@@ -15,6 +16,30 @@ namespace LearnCSharpMySQL1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string mycon = "datasource=localhost;port=3306;username=root;Password=2557";
+                MySqlConnection con = new MySqlConnection(mycon);
+                MySqlDataAdapter adp = new MySqlDataAdapter();
+                adp.SelectCommand = new MySqlCommand("SELECT * database.EmployeeInfo",con);
+                //MySqlCommandBuilder cb = new MySqlCommandBuilder(adp);
+
+                con.Open();
+
+                MessageBox.Show("connected");
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
