3f7045357ea5d2f1308a887dffd7cbf62b1dd286
Madazer <codermaz@outlook.com>
2016-01-12 18:55:40 +0000

    Add items to database(Successful)

--- LearnCSharpMySQL1/Form2.cs
+++ LearnCSharpMySQL1/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace LearnCSharpMySQL1
 {
@@ -20,6 +21,28 @@ namespace LearnCSharpMySQL1
         private void Form2_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mycon = "datasource=localhost;port=3306;username=root;Password=2557";
+            string Query = "INSERT INTO database.players (idPlayers,UserName,Password,Goals,Assist) VALUES ('"+this.id.Text+"','"+user.Text+"','"+pass.Text+"','"+goal.Text+"','"+assist.Text+"');";
+
+            MySqlConnection con = new MySqlConnection(mycon);
+            MySqlCommand cmd = new MySqlCommand(Query, con);
+            MySqlDataReader myReader;
+
+            try
+            {
+                con.Open();
+                myReader = cmd.ExecuteReader();
+                MessageBox.Show("Saved");
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
